06e2ee7445bea0adf1f705e408e66b533ef653be
deployify <oscar@jaha.it>
2020-07-30 23:08:59 +0200

    new installer

--- scripts/deploy_proxy_container.sh
+++ scripts/deploy_proxy_container.sh
@@ -3,6 +3,7 @@
 CONTAINER=deployify_proxy
 IMAGE=deployify/proxy:1.0.0
 DATA=/var/lib/deployify/data
+USER_GROUP=$(id -u):$(id -g)
 
 echo Updating $IMAGE...
 docker pull $IMAGE
@@ -11,4 +12,4 @@ echo Removing $CONTAINER...
 docker rm $CONTAINER -v -f
 
 echo Initiating $CONTAINER...
-docker run --name $CONTAINER --network deployify -p80:80 -p81:81 -p443:443 -d -v $DATA/config:/config -ti $IMAGE
+docker run --name $CONTAINER --network deployify --user $USER_GROUP -p80:80 -p81:81 -p443:443 -d -v $DATA/config:/config -ti $IMAGE
