2b1c63c6146b39c47acdaf558fac4096b4b7644d
deployify <oscar@jaha.it>
2020-07-31 18:08:15 +0200

    new installer

--- scripts/deploy_portainer_container.sh
+++ scripts/deploy_portainer_container.sh
@@ -4,3 +4,4 @@ docker volume create portainer_data
 docker pull portainer/portainer
 docker rm -f portainer
 docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+docker image prune -af --filter "label!=portainer"
