711a6add2e877e0501d84c194bc8e419127cb247
deployify <oscar@jaha.it>
2020-08-04 21:55:27 +0200

    new installer

--- scripts/deploy_neo4j_container.sh
+++ scripts/deploy_neo4j_container.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 CONTAINER=deployify_neo4j
-IMAGE=neo4j:3.5
+IMAGE=neo4j:4.1
 DATA=/var/lib/deployify/data
 USER_GROUP=$(id -u):$(id -g)
 
@@ -12,4 +12,4 @@ echo "Removing $CONTAINER..."
 sudo docker rm $CONTAINER -f
 
 echo "Initiating $CONTAINER..."
-sudo docker run --name $CONTAINER --network deployify --user $USER_GROUP --restart=always -p7474:7474 -p7687:7687 -d -v $DATA/neo4j:/data --env NEO4J_AUTH=neo4j/p@ssw0rd $IMAGE
+sudo docker run --name $CONTAINER --network deployify --user $USER_GROUP --restart=always -p7474:7474 -p7687:7687 -d -v $DATA/neo4j:/data --env NEO4J_AUTH=neo4j/p@ssw0rd --env NEO4J_dbms_allow__upgrade=true $IMAGE
