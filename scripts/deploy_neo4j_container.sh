60603bd8021ff7c4607a40b4aada98fa80e916d4
deployify <oscar@jaha.it>
2020-08-19 12:13:35 +0200

    New installer - added env for neo4j container

--- scripts/deploy_neo4j_container.sh
+++ scripts/deploy_neo4j_container.sh
@@ -12,4 +12,4 @@ echo "Removing $CONTAINER..."
 sudo docker rm $CONTAINER -f
 
 echo "Initiating $CONTAINER..."
-sudo docker run --name $CONTAINER --network deployify --user $USER_GROUP --restart=always -p7474:7474 -p7687:7687 -d -v $DATA/neo4j:/data --env NEO4J_AUTH=neo4j/p@ssw0rd --env NEO4J_dbms_allow__upgrade=true $IMAGE
+sudo docker run --name $CONTAINER --network deployify --user $USER_GROUP --restart=always -p7474:7474 -p7687:7687 -d -v $DATA/neo4j:/data --env NEO4J_AUTH=neo4j/p@ssw0rd --env NEO4J_dbms_allow__upgrade=true --env NEO4J_cypher_lenient__create__relationship=true $IMAGE
