090a50dc54b2943520cdc2a20808ecf565728ec2
deployify <oscar@jaha.it>
2020-08-25 11:47:08 +0200

    Migration script

---
+++ scripts/migrate_neo4j_4.0.0.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+CONTAINER=deployify_neo4j
+IMAGE=neo4j:4.0.0
+DATA=/var/lib/deployify/data
+USER_GROUP=$(id -u):$(id -g)
+
+echo "Stopping $CONTAINER..."
+sudo docker stop $CONTAINER
+
+echo "Removing $CONTAINER..."
+sudo docker rm $CONTAINER -f
+
+echo "Initiating $CONTAINER..."
+sudo docker run --name $CONTAINER --network deployify --user $USER_GROUP --restart=always -p7474:7474 -p7687:7687 -ti -v $DATA/neo4j:/data --env NEO4J_AUTH=neo4j/p@ssw0rd --env NEO4J_dbms_allow__upgrade=true --env NEO4J_cypher_lenient__create__relationship=true $IMAGE
